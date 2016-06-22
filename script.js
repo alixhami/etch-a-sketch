ebc7292852eba9ad11232713a0d1c48ed454b0a3
alixhami <alix.hamilton@gmail.com>
2016-06-22 14:42:26 -0700

    update js for game frame

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var tableSize = 0;
+
 $(document).ready(function(){
-	$('button').click(function(){
-  	$('#grid-container').empty();
+  $('button').click(function(){
+    $('#grid-container').empty();
     var table = "<table>";
     function addTable(size){
       var tableRow = "";
@@ -13,12 +14,45 @@ $(document).ready(function(){
       }
       table += "</table>";
     }
-  	var tableSize = prompt("How big?");
+
+    //Make the thing
+    var tableSize = prompt("How big?");
     addTable(tableSize);
     $('#grid-container').prepend(table);
     $('td').addClass("squares");
+    $('#container').css('visibility','visible');
+
+    //Define them variables
+    var frameWidth = $('#frame').width();
+    console.log(frameWidth);
+    var sqWidth = frameWidth / tableSize;
+    var sqHeight = sqWidth;
+
+    //Size the thang
+    $('#frame').css({
+      'height': frameWidth
+    });
+    $('#canvas').css({
+      'width': frameWidth*.8,
+      'height': frameWidth*.8
+    });    
+    $('#grid-container').css({
+      'width':frameWidth*.8,
+      'height':frameWidth*.8
+    });
+    $('table').css({
+      'width':frameWidth*.8,
+      'height':frameWidth*.8
+    })
+    $('.circles').css({
+      'width':frameWidth/12,
+      'height':frameWidth/12
+    });
+    $('p').css('font-size',frameWidth/11);
+
+    //Mouseover Definition
     $('.squares').mouseenter(function(){
-  		$(this).css('background-color','black');
-  	});
+  	$(this).css('background-color','black');
+    });
   });
 });
